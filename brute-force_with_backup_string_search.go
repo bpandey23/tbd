74d0559d79615357b352d7c17c0c705e6a31c622
bpandey23 <107094937+bpandey23@users.noreply.github.com>
2022-08-18 09:18:51 +0530

    With backup (#3)
    * backup approach
    
    * used byte
    
    Co-authored-by: Bhavesh Pandey <bhavesh.pandey@dxc.com>

--- brute-force_with_backup_string_search.go
+++ brute-force_with_backup_string_search.go
@@ -14,8 +14,10 @@ func main() {
 func brute_force_with_backup_string_search(pat, txt string) (start, end int, err error) {
 	n := len(txt)
 	m := len(pat)
-	p_r := []rune(pat)
-	t_r := []rune(txt)
+
+	//commented rune conversion , due to its limited scope
+	p_r := []byte(pat) // []rune(pat)
+	t_r := []byte(txt) //[]rune(txt)
 
 	var i, j int
 	for i, j = 0, 0; i < n && j < m; i++ {
